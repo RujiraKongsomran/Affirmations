f57c98bff981c5b8f5947161eabed1ef46cb6b4d
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-11-30 21:15:00 +0700

    Create Interface

---
+++ app/src/main/java/com/rujirakongsomran/affirmations/Interface/JsonPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.rujirakongsomran.affirmations.Interface;
+
+import com.rujirakongsomran.affirmations.Model.Data;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("")
+    Call<Data> getData();
+}
