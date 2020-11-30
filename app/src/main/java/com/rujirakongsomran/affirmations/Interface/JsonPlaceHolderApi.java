951b962ff66a314296c929f9f4544edb8537d5cd
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-11-30 21:22:32 +0700

    Call Service

--- app/src/main/java/com/rujirakongsomran/affirmations/Interface/JsonPlaceHolderApi.java
+++ app/src/main/java/com/rujirakongsomran/affirmations/Interface/JsonPlaceHolderApi.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface JsonPlaceHolderApi {
-    @GET("")
+    @GET("/")
     Call<Data> getData();
 }
--- app/src/main/java/com/rujirakongsomran/affirmations/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/affirmations/MainActivity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.rujirakongsomran.affirmations.Interface.JsonPlaceHolderApi;
+import com.rujirakongsomran.affirmations.Model.Data;
 import com.rujirakongsomran.affirmations.databinding.ActivityMainBinding;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.affirmations.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<Data> call = jsonPlaceHolderApi.getData();
+
+        call.enqueue(new Callback<Data>() {
+            @Override
+            public void onResponse(Call<Data> call, Response<Data> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "onResponse()", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Data> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "onFailure()", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
