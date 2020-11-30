e9d735bd4ab566d0a4af83e909193a4048a72dd0
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-11-30 21:12:31 +0700

    Create Model

---
+++ app/src/main/java/com/rujirakongsomran/affirmations/Model/Data.java
@@ -0,0 +1,20 @@
+package com.rujirakongsomran.affirmations.Model;
+
+public class Data {
+    private String affirmation;
+
+    public Data() {
+    }
+
+    public Data(String affirmation) {
+        this.affirmation = affirmation;
+    }
+
+    public String getAffirmation() {
+        return affirmation;
+    }
+
+    public void setAffirmation(String affirmation) {
+        this.affirmation = affirmation;
+    }
+}
